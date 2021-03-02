65a50d3d7c9cf3866f2e4f68442c92970c5ff2eb
Richard Tae <taerichard@gmail.com>
2021-03-01 20:45:52 -0800

    created entities with pk and columninfo

--- app/src/main/java/com/example/calmthestorm/roombdb/db.java
+++
@@ -1,5 +0,0 @@
-package com.example.calmthestorm.roombdb;
-
-//@Database(entities = User.class, Location.class, Disaster.class, FemaInfo.class, SurvivalKit.class, SurvivalMaps.class)
-//public class disasterDb {
-//}
