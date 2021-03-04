66f727a1b1f4d54bf9de810a9646904e228f6513
Matin Mirzaei <45024401+MatinMirzaei@users.noreply.github.com>
2021-03-03 20:21:01 -0800

    Map created

---
+++ app/src/main/java/com/example/calmthestorm/Map.java
@@ -0,0 +1,16 @@
+package com.example.calmthestorm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps_survival);
+    }
+}
--- app/src/main/java/com/example/calmthestorm/SurvivalMaps.java
+++ app/src/main/java/com/example/calmthestorm/SurvivalMaps.java
@@ -1,8 +1,12 @@
 package com.example.calmthestorm;
 
+
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+
+import java.util.ArrayList;
+
 
 @Entity
 public class SurvivalMaps {
